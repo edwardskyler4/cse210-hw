7780c8a08d47f1d50a75cb378168bb04918ec5d2
Kyler Edwards <edwardskyler4@gmail.com>
2026-01-10 20:08:09 -0700

    Modify: accomplishing prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? ");
+        string grade = Console.ReadLine();
+        float numGrade = float.Parse(grade);
+
+        string letterGrade = "";
+        string signGrade = "";
+
+        // Letter grade
+        if (numGrade > 89)
+        {
+            letterGrade = "A";
+        }
+        else if (numGrade > 79)
+        {
+            letterGrade = "B";
+        }
+        else if (numGrade > 69)
+        {
+            letterGrade = "C";
+        }
+        else if (numGrade > 59)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        // Sign grade
+        if (numGrade % 10 >= 7)
+        {
+            signGrade = "+";
+        }
+        else if (numGrade % 10 <= 3)
+        {
+            signGrade = "-";
+        }
+        else
+        {
+            signGrade = "";
+        }
+
+        // Print logic
+        if (letterGrade == "A" && signGrade != "-" || letterGrade == "F")
+        {
+            Console.WriteLine($"Your grade is {letterGrade}");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letterGrade}{signGrade}");
+        }
+
+        if (numGrade >= 70)
+        {
+            Console.WriteLine("You passed! Good Job!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. You can do it if you try again though!");
+        }
     }
 }
