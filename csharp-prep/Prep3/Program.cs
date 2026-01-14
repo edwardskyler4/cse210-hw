751204a0867451e382399882e90433637cae5c29
Kyler Edwards <edwardskyler4@gmail.com>
2026-01-14 15:27:32 -0700

    completing prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool play = true;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessCount = 0;
+
+            do
+            {
+                guessCount ++;
+
+                Console.WriteLine("Enter your guess: ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > magicNum)
+                {
+                 Console.WriteLine("Lower");   
+                }
+                else if (guess < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"Correct! It took you {guessCount} guesses");
+                }
+            } while (guess != magicNum);
+
+            Console.WriteLine("Would you like to play again? y/n");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == "n")
+            {
+                play = false;
+            }
+        } while (play == true);
+
+        Console.Write("Thanks for playing!");
     }
 }
