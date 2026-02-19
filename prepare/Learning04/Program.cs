dfd1e35f04450acb15687a019ebcd13c03972c8d
Kyler Edwards <edwardskyler4@gmail.com>
2026-02-19 13:01:12 -0700

    Learning: complete homework learning assignnment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new("Samuel Bennett", "Multiplication");
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
+        Console.WriteLine();
+
+        MathAssignment mAss = new("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        string mSummary = mAss.GetSummary();
+        string homeworkList = mAss.GetHomeworkList();
+        Console.WriteLine(mSummary);
+        Console.WriteLine(homeworkList);
+        Console.WriteLine();
+
+        WritingAssignment wAss = new("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        string wSummary = wAss.GetSummary();
+        string writingInformation = wAss.GetWritingInformation();
+        Console.WriteLine(wSummary);
+        Console.WriteLine(writingInformation);
     }
 }
