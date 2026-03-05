02e7b69aa52d84821d691e855185973ba78f9554
Kyler Edwards <edwardskyler4@gmail.com>
2026-03-05 13:36:13 -0700

    Learning: complete shapes assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+        Square square = new("blue", 5);
+        Rectangle rectangle = new("yellow", 3, 4);
+        Circle circle = new("pink", 1);
+
+        shapes.AddRange(square, rectangle, circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()} - {shape.GetArea()}");
+        }
+
     }
 }
